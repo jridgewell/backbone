163370f3588db8bf3fe8e7044d447a7ebbc71652
Justin Ridgewell <justin@ridgewell.name>
2015-05-23 04:01:54 -0400

    Declare the var when we use it

--- backbone.js
+++ backbone.js
@@ -585,9 +585,8 @@
     // If the server returns an attributes hash that differs, the model's
     // state will be `set` again.
     save: function(key, val, options) {
-      var attrs, method, xhr, attributes = this.attributes, wait;
-
       // Handle both `"key", value` and `{key: value}` -style arguments.
+      var attrs;
       if (key == null || typeof key === 'object') {
         attrs = key;
         options = val;
@@ -596,7 +595,8 @@
       }
 
       options = _.extend({validate: true, parse: true}, options);
-      wait = options.wait;
+      var wait = options.wait;
+      var attributes = this.attributes;
 
       // If we're not waiting and attributes exist, save acts as
       // `set(attr).save(null, opts)` with validation. Otherwise, check if
@@ -629,9 +629,9 @@
       };
       wrapError(this, options);
 
-      method = this.isNew() ? 'create' : (options.patch ? 'patch' : 'update');
+      var method = this.isNew() ? 'create' : (options.patch ? 'patch' : 'update');
       if (method === 'patch' && !options.attrs) options.attrs = attrs;
-      xhr = this.sync(method, this, options);
+      var xhr = this.sync(method, this, options);
 
       // Restore attributes.
       if (attrs && wait) this.attributes = attributes;
